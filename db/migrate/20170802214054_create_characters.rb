50b98ba943ef4004514a186b2fe8fb801e236036
Jacob Greenwald <jeg4t@virginia.edu>
2017-08-02 18:10:02 -0400

    seeded

--- db/migrate/20170802214054_create_characters.rb
+++ db/migrate/20170802214054_create_characters.rb
@@ -2,6 +2,7 @@ class CreateCharacters < ActiveRecord::Migration[5.1]
   def change
     create_table :characters do |t|
       t.string :char_name
+      t.string :photo_url
       t.references :house, index: true, foreign_key: true
     end
   end
