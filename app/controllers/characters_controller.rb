9d3087e6c87cae9a381fc5fa323f62e1349c9913
Jacob Greenwald <jeg4t@virginia.edu>
2017-08-02 21:03:11 -0400

    add new, create and show paths for characters

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,2 +1,31 @@
 class CharactersController < ApplicationController
+  #new
+  def new
+    @house = House.find(params[:house_id])
+    @character = @house.characters.new
+  end
+
+  #create
+  def create
+    @house = House.find(params[:house_id])
+    @character = @house.characters.create(character_params)
+    redirect_to @house
+  end
+
+  #show
+  def show
+    @house = House.find(params[:house_id])
+    @character = Character.find(params[:id])
+  end
+  #edit
+
+
+
+  #update
+
+  #destroy
+  private
+  def character_params
+    params.require(:character).permit(:char_name, :photo_url)
+  end
 end
