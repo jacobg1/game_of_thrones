20bc21dce14dac51f73821fd954960093683f5ad
Jacob Greenwald <jeg4t@virginia.edu>
2017-08-02 20:13:56 -0400

    add new house path

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -2,7 +2,32 @@ class HousesController < ApplicationController
   def index
     @houses = House.all
   end
+
   def show
     @house = House.find(params[:id])
+  end
+  #edit
+  def edit
+
+  end
+  #new
+  def new
+    @house = House.new
+  end
+  #create
+  def create
+    @house = House.create(house_params)
+    redirect_to house_path(@house)
+  end
+  #update
+  def update
+  end
+  #destroy
+  def destroy
+  end
+
+  private
+  def house_params
+    params.require(:house).permit(:house_name, :moto, :sigil)
   end
 end
